5fbc37e43006693d75be3b1b27f24979c920370f
Andrei <andreicm@ntnu.no>
2024-10-01 16:25:23 +0200

    Ulike codesnippets

---
+++ Canvas_webtek/Exercise_2.js
@@ -0,0 +1,9 @@
+// const button = document.getElementById("button");
+function skriveut(){
+    window.print(); /*Den bruker funksjonaliteten for å skriveut.*/
+    // let html = document.body.innerHTML;
+    // return console.log(html);
+/* Det ser ut at EventListener metoden fungerer ikke i en funksjon.*/
+}
+// button.addEventListener("click", skriveut())
+//Uten event Listener vil den fungere.
